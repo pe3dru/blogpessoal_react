8e54ca98a218faec22ed3fc9f330db4c8591b06b
pe3dru <lionpedro30@gmail.com>
2025-10-23 16:50:27 -0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./footer/Footer";
 import Home from "./pages/home/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+      </>
+  );
 }
 
 export default App;
