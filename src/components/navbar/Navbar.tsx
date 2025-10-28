477312d6b0ca28f237c560131a223282018d33ca
pe3dru <lionpedro30@gmail.com>
2025-10-28 12:53:52 -0300

    lógica da página de login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
+
+  const navigate = useNavigate();
+  
+  const { handleLogout } = useContext(AuthContext)
+  
+  function logout () {
+  
+    handleLogout ()
+    alert("O Usuário foi desconectado com sucesso!")
+    navigate('/')
+  }
   return (
     <>
       <div className='w-full flex justify-center py-4
-      bg-indigo-900 text-white'>
+                     bg-indigo-900 text-white'>
 
         <div className="container flex justify-between text-lg mx-8">
           <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
@@ -15,7 +28,7 @@ function Navbar() {
             Cadastrar 
             Tema
             Perfil
-            Sair
+            <Link to= '' onClick={logout} className='hover:underline'>Sair</Link>
           </div>
         </div>
       </div>
