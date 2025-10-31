2d09033b587c1ef3b9270975b439173a1d7bab0c
pe3dru <lionpedro30@gmail.com>
2025-10-31 17:48:25 -0300

    crud do blog pessoal finalizado

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,86 +1,39 @@
-function Home () {
-  return (
-    <div
-      style={{
-        backgroundColor: "#312e41",
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          color: "white",
-          width: "100%",
-          maxWidth: "1280px",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            textAlign: "center",
-            paddingTop: "lrem",
-            paddingBottom: "lrem",
-          }}
-        >
-          <h2
-            style={{
-              fontSize: "3rem",
-              fontWeight: "bold",
-            }}
-          >
-            Tu de novo?😒
-          </h2>
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem"
 
-          <p
-            style={{
-              fontSize: "1.25rem",
-            }}
-          >
-            Manda a boa
-          </p>
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Tu de novo?
+                        </h2>
+                        <p className='text-xl'>
+                            Manda a boa!
+                        </p>
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              gap: "lrem",
-            }}
-          >
-            <div
-              style={{
-                borderRadius: "0.5rem",
-                cursor: "pointer",
-                border: "2px solid white",
-                padding: "0.5rem 1rem",
-              }}
-            >
-              Nova Postagem
+                        <div className="flex justify-around gap-4">
+                            
+                            <ModalPostagem />
+
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://i.imgur.com/fyfri1v.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <img
-            src="https://i.imgur.com/fyfri1v.png"
-            alt="Imagem Página Home"
-            style={{
-              width: "90%",
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  )
+            
+            <ListaPostagens />
+        </>
+    )
 }
 
 export default Home
