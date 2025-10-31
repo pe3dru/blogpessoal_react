2b84a27953dc59fa339f9744eabdaa8cfd24717c
pe3dru <lionpedro30@gmail.com>
2025-10-31 18:34:09 -0300

    Página de Perfil

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,14 @@ import DeletarTema from "./components/deletertema/DeletarTema";
 import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens";
 import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
 import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
+import Perfil from "./pages/perfil/Perfil";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -30,6 +34,7 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
            </Routes>
           </div>
           <Footer />
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,29 +1,43 @@
 import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react"
+import { useContext, type ReactNode } from "react"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Footer() {
+
   let data = new Date().getFullYear()
 
-  return (
-    <>
+  const { usuario } = useContext(AuthContext)
+
+  let component: ReactNode
+
+  if (usuario.token !== "") {
+
+    component = (
       <div className="flex justify-center bg-indigo-900 text-white">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">
-            Blog de Pedro |  {data}
+            Blog Pessoal Generation | Copyright: {data}
           </p>
-          <p className="text-lg">Acesse minhas redes sociais</p>
+          <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <a href="https://www.linkedin.com/in/pedroemanueln" target="_blank">
-                <LinkedinLogoIcon size={48} weight="bold"/>
+            <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+              <LinkedinLogoIcon size={48} weight='bold' />
             </a>
-            <a href="https://www.instagram.com/pre3du" target="_blank">
-                <InstagramLogoIcon size={48} weight="bold"/>
+            <a href="https://www.instagram.com/seu_usuario" target="_blank">
+              <InstagramLogoIcon size={48} weight='bold' />
             </a>
-            <a href="https://www.facebook.com/pedro.emanuel.16503" target="_blank">
-                <FacebookLogoIcon size={48} weight="bold"/>
+            <a href="https://www.facebook.com/seu_usuario" target="_blank">
+              <FacebookLogoIcon size={48} weight='bold' />
             </a>
-            </div>
+          </div>
         </div>
       </div>
+    )
+  }
+
+  return (
+    <>
+      { component }
     </>
   )
 }
